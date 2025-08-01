57399b1bc72a93b8109dcc9e31a573e47331da46
GitHub Action <action@github.com>
2025-08-01 10:58:53 +0000

    Template cleanup

--- src/main/kotlin/com/github/x9917638/matugenforintellij/MyBundle.kt
+++ src/main/kotlin/com/github/x9917638/matugenforintellij/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.x9917638.matugenforintellij
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/x9917638/matugenforintellij/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/x9917638/matugenforintellij/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.x9917638.matugenforintellij.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
